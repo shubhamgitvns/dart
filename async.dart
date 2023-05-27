660c03d130f0e20269ce7b4d8d58b86822972f8a
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-27 12:48:19 +0530

    Update async.dart

--- async.dart
+++ async.dart
@@ -2,10 +2,12 @@ void main() async {
   dynamic retvalue = asyncFunction();
   print("Value = $retvalue, Type= ${retvalue.runtimeType}");
   retvalue = await asyncFunction();
+  print("Value = $retvalue}");
   print("Value = $retvalue, Type= ${retvalue.runtimeType}");
 }
 
 Future asyncFunction() async {
-  await Future.delayed(Duration(seconds: 5));
+  //Demo function for a long running function in another thread like API call.
+  await Future.delayed(Duration(seconds: 10));
   return 'Return from Async Function';
 }
