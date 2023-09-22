d892ad807bea38f45cfb2d4ee6542422e7479fa1
Shubham kumar Mourya <93187281+shubhamgitvns@users.noreply.github.com>
2023-09-22 13:40:48 +0530

    Update Inheritance.dart
    Changed the toString method

--- Inheritance.dart
+++ Inheritance.dart
@@ -1,28 +1,36 @@
 void main() {
-Employee e=Employee(name:"Sachin",address:"Mumbai",age:50,
-                    post: "Batsman",salary:1000 );
+  Employee e = Employee(
+      name: "Sachin",
+      address: "Mumbai",
+      age: 50,
+      post: "Batsman",
+      salary: 1000);
   print(e);
 }
-class Person{
-  String name,address;
+
+class Person {
+  String name, address;
   int age;
-  Person({required this.name,required this.address,required this.age});
+  Person({required this.name, required this.address, required this.age});
   @override
-  String toString()
-  {
-    return "Person($name)";
+  String toString() {
+    return "Person(Name: $name, Address: $address,Age: $age)";
   }
 }
-class Employee extends Person
-{
+
+class Employee extends Person {
   String post;
   int salary;
-  Employee({required name,required address,required age,required this.post,required this.salary})
-    :super(name:name,age:age,address:address);
-  
-    @override
-  String toString()
-  {
-    return  " Employee(${super.toString()}, $post)";
+  Employee(
+      {required name,
+      required address,
+      required age,
+      required this.post,
+      required this.salary})
+      : super(name: name, age: age, address: address);
+
+  @override
+  String toString() {
+    return " Employee(${super.toString()},Post: $post)";
   }
 }
